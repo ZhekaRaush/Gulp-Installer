2eb0d1dcd1b7c1e195445ea2f0cf72011bed4dd7
Zheka@RAUSHWEB <rausch08@yandex.ru>
2023-01-24 01:13:35 +0300

    Добавлен плагин gulp-size:   gulpfile.js Добавлен плагин gulp-size:   package-lock.json Добавлен плагин gulp-size:   package.json

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,17 @@ const { src, dest } = require('gulp');
 // Обработка HTML подключение HTML файлов
 const fileinclude = require('gulp-file-include');
 const htmlmin = require('gulp-htmlmin');
+const size = require('gulp-size');
 
 // Модули
 // Обработка HTML
 const html = (cb) => {
-    console.log('Обработка HTML');
+    console.log('Обработка HTML <=======================================>');
     return src('./src/html/*.html')
         .pipe(fileinclude())
+        .pipe(size({ title: 'До сжатия'}))
         .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(size({ title: 'После сжатия'}))
         .pipe(dest('./public'));
 };
 
