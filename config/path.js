8b1037f2bb16dc38550f966bfb082944f28f2676
Zheka@RAUSHWEB <rausch08@yandex.ru>
2023-01-24 15:50:00 +0300

    Файл с настройками путей к файлам:   config/path.js

---
+++ config/path.js
@@ -0,0 +1,19 @@
+// Путь к файлам
+const pathSrc = "./src";
+const pathDest = "./public";
+
+module.exports = {
+    root: pathDest,
+
+    html: {
+        src: pathSrc + "/html/*.html",
+        watch: pathSrc + "/html/**/*.html",
+        dest: pathDest
+    },
+
+    pug: {
+        src: pathSrc + "/pug/*.pug",
+        watch: pathSrc + "/pug/**/*.pug",
+        dest: pathDest
+    }
+};
