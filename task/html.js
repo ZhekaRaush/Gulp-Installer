ff6ac1d9cdb0e610ca83cc520b706fa565645a02
Zheka@RAUSHWEB <rausch08@yandex.ru>
2023-01-24 15:55:15 +0300

    Подключил конфигурационный файл ./config/path.js :   task/html.js

--- task/html.js
+++ task/html.js
@@ -1,5 +1,7 @@
 // Подключение основного модуля GULP
 const { src, dest } = require('gulp');
+// Конфигурация
+const path = require('../config/path.js');
 // Плагины
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
@@ -9,7 +11,7 @@ const size = require('gulp-size');
 
 const html = () => {
     console.log('Обработка HTML <=======================================>');
-    return src('./src/html/*.html')
+    return src(path.html.src)
         .pipe(plumber({
             errorHandler: notify.onError(error => ({
                 title:"HTML",
@@ -20,6 +22,6 @@ const html = () => {
         .pipe(size({ title: 'До сжатия'}))
         .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(size({ title: 'После сжатия'}))
-        .pipe(dest('./public'));
+        .pipe(dest(path.html.dest));
 };
 module.exports = html;
