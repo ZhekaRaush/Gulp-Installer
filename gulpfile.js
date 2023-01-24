af8ffb672abbdb6d71adeae19beb27e91b3718e8
Zheka@RAUSHWEB <rausch08@yandex.ru>
2023-01-24 16:44:39 +0300

    Подключил ./config/path.js н настроил пути к файлам:   gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 // Подключение основного модуля GULP
 const { watch, series, parallel } = require('gulp');
 const browserSync = require('browser-sync').create();
+// Конфигурация
+const path = require('./config/path.js');
 
 // Удаление директорий
 const clear = require('./task/clear.js');
@@ -14,15 +16,15 @@ const pug = require('./task/pug.js');
 const server = () => {
     browserSync.init({
         server: {
-            baseDir: './public'
+            baseDir: path.root
         }
     })
 };
 
 // Наблюдение
 const watcher = () => {
-    watch('./src/html/**/*.html', html).on("all", browserSync.reload);
-    watch('./src/pug/**/*.pug', pug).on("all", browserSync.reload);
+    watch(path.html.watch, html).on("all", browserSync.reload);
+    watch(path.pug.watch, pug).on("all", browserSync.reload);
 };
 
 // Задачи
