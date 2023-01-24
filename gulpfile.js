8309a4ed623fbc55236f2d6812f791f7c2c57aee
Zheka@RAUSHWEB <rausch08@yandex.ru>
2023-01-24 14:16:42 +0300

    Создал задачу для обработки PUG файлов:   gulpfile.js Установил плагин gulp-pug:   package-lock.json Установил плагин gulp-pug:   package.json

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ const notify = require('gulp-notify');
 const fileinclude = require('gulp-file-include');
 const htmlmin = require('gulp-htmlmin');
 const size = require('gulp-size');
+const pugs = require('gulp-pug');
 
 // Модули
 // Обработка HTML
@@ -26,6 +27,26 @@ const html = (cb) => {
         .pipe(size({ title: 'До сжатия'}))
         .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(size({ title: 'После сжатия'}))
+        .pipe(dest('./public'))
+        .pipe(browserSync.stream());
+};
+
+// Обработка PUG
+const pug = (cb) => {
+    console.log('Обработка PUG <=======================================>');
+    return src('./src/pug/*.pug')
+        .pipe(plumber({
+            errorHandler: notify.onError(error => ({
+                title:"Pug",
+                message:error.message
+            }))
+        }))
+        .pipe(pugs({
+            pretty: true,
+            data: {
+                news: require('./data/news.json')
+            }
+        }))
         .pipe(dest('./public'))
         .pipe(browserSync.stream());
 };
@@ -47,16 +68,18 @@ const server = () => {
 // Наблюдение
 const watcher = () => {
     watch('./src/html/**/*.html', html);
+    watch('./src/pug/**/*.pug', pug);
 };
 
 // Задачи
 exports.html = html;
+exports.pug = pug;
 exports.watcher = watcher;
 exports.clear = clear;
 
 // Сборка проекта
 exports.dev = series(
     clear,
-    html,
+    pug,
     parallel(watcher, server)
 );
