7d796bff00e1595bac77ce305f6ee400a8e307c3
Zheka@RAUSHWEB <rausch08@yandex.ru>
2023-01-24 17:59:02 +0300

    Внес так же необходимве коректировки для PUG файлов:   config/app.js Внес так же необходимве коректировки для PUG файлов:   task/pug.js

--- task/pug.js
+++ task/pug.js
@@ -2,6 +2,7 @@
 const { src, dest } = require('gulp');
 // Конфигурация
 const path = require('../config/path.js');
+const app = require('../config/app.js');
 // Плагин
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
@@ -16,12 +17,7 @@ const pug = () => {
                 message:error.message
             }))
         }))
-        .pipe(pugs({
-            pretty: true,
-            data: {
-                news: require('../data/news.json')
-            }
-        }))
+        .pipe(pugs(app.pug))
         .pipe(dest(path.pug.dest));
 };
 module.exports = pug;
